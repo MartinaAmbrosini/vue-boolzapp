c0aa42f3018070dbd01faa05c7b37965aba6f4d4
Martina Ambrosini <martinaambrosini17@gmail.com>
2024-01-04 20:26:28 +0100

    add: aggiunte funzionalità di vue e js e finito esercizio

--- js/script.js
+++ js/script.js
@@ -5,83 +5,87 @@ createApp({
         return {
             contacts: [
                 {
-                    name: 'Michele',
-                    avatar: './img/avatar_1.png',
+                    name: 'Bin',
+                    avatar: './img/avatar_bin.png',
+                    filter: true,
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '15:30',
                             message: 'Hai portato a spasso il cane?',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '15:50',
                             message: 'Ricordati di stendere i panni',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 16:15:22',
+                            date: '16:15',
                             message: 'Tutto fatto!',
                             status: 'received'
                         }
                     ],
                 },
                 {
-                    name: 'Fabio',
-                    avatar: './img/avatar_2.png',
+                    name: 'Lyney',
+                    avatar: './img/avatar_lyney.png',
+                    filter: true,
                     visible: false,
                     messages: [
                         {
-                            date: '20/03/2020 16:30:00',
+                            date: '16:30',
                             message: 'Ciao come stai?',
                             status: 'sent'
                         },
                         {
-                            date: '20/03/2020 16:30:55',
+                            date: '16:30',
                             message: 'Bene grazie! Stasera ci vediamo?',
                             status: 'received'
                         },
                         {
-                            date: '20/03/2020 16:35:00',
+                            date: '16:35',
                             message: 'Mi piacerebbe ma devo andare a fare la spesa.',
                             status: 'sent'
                         }
                     ],
                 },
                 {
-                    name: 'Samuele',
-                    avatar: './img/avatar_3.png',
+                    name: 'Paimon',
+                    avatar: './img/avatar_paimon.png',
+                    filter: true,
                     visible: false,
                     messages: [
                         {
-                            date: '28/03/2020 10:10:40',
+                            date: '10:10',
                             message: 'La Marianna va in campagna',
                             status: 'received'
                         },
                         {
-                            date: '28/03/2020 10:20:10',
+                            date: '10:20',
                             message: 'Sicuro di non aver sbagliato chat?',
                             status: 'sent'
                         },
                         {
-                            date: '28/03/2020 16:15:22',
+                            date: '16:15',
                             message: 'Ah scusa!',
                             status: 'received'
                         }
                     ],
                 },
                 {
-                    name: 'Alessandro B.',
-                    avatar: './img/avatar_4.png',
+                    name: 'Yae',
+                    avatar: './img/avatar_yae.png',
+                    filter: true,
                     visible: false,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '15:30',
                             message: 'Lo sai che ha aperto una nuova pizzeria?',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '15:50',
                             message: 'Si, ma preferirei andare al cinema',
                             status: 'received'
                         }
@@ -89,16 +93,17 @@ createApp({
                 },
                 {
                     name: 'Alessandro L.',
-                    avatar: './img/avatar_5.png',
+                    avatar: './img/avatar_default.jpg',
+                    filter: true,
                     visible: false,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '15:30',
                             message: 'Ricordati di chiamare la nonna',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '15:50',
                             message: 'Va bene, stasera la sento',
                             status: 'received'
                         }
@@ -106,21 +111,22 @@ createApp({
                 },
                 {
                     name: 'Claudia',
-                    avatar: './img/avatar_6.png',
+                    avatar: './img/avatar_default.jpg',
+                    filter: true,
                     visible: false,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '15:30',
                             message: 'Ciao Claudia, hai novità?',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '15:50',
                             message: 'Non ancora',
                             status: 'received'
                         },
                         {
-                            date: '10/01/2020 15:51:00',
+                            date: '15:51',
                             message: 'Nessuna nuova, buona nuova',
                             status: 'sent'
                         }
@@ -128,38 +134,40 @@ createApp({
                 },
                 {
                     name: 'Federico',
-                    avatar: './img/avatar_7.png',
+                    avatar: './img/avatar_default.jpg',
+                    filter: true,
                     visible: false,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '15:30',
                             message: 'Fai gli auguri a Martina che è il suo compleanno!',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '15:50',
                             message: 'Grazie per avermelo ricordato, le scrivo subito!',
                             status: 'received'
                         }
                     ],
                 },
                 {
-                    name: 'Davide',
-                    avatar: './img/avatar_8.png',
+                    name: 'Filippo',
+                    avatar: './img/avatar_default.jpg',
+                    filter: true,
                     visible: false,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '15:30',
                             message: 'Ciao, andiamo a mangiare la pizza stasera?',
                             status: 'received'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '15:50',
                             message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:51:00',
+                            date: '15:51',
                             message: 'OK!!',
                             status: 'received'
                         }
@@ -169,4 +177,52 @@ createApp({
 
         }
     },
+    methods: {
+        /* funzione che cambia la chat visualizzata dopo averla clickata dalla lista */
+        active(i) {
+            // metto l'attributo visible a false per ogni contatto nell'array contacts
+            for (let index = 0; index < this.contacts.length; index++) {
+                this.contacts[index].visible = false;
+            }
+
+            //imposto l'attributo visible a true per il contatto che ho cliccato
+            this.contacts[i].visible = true;
+        },
+        /* funzione che manda il messaggio e una risposta nella chat */
+        invia(msg) {
+            //cerco il contatto con visible a true
+            for (let i = 0; i < this.contacts.length; i++) {
+                let temp = this.contacts[i]; //variabile temporanea (con visibilità solo nel for) per non scrivere sempre this.contacts[i]
+                if (temp.visible) {
+                    //creo l'oggetto messaggio da aggiungere in coda all'array messages
+                    let time = new Date();
+                    const messageSent = {date: time.getHours() + ':' + time.getMinutes(), message: msg, status: 'sent'};
+
+                    //aggiungo il messaggio
+                    temp.messages.push(messageSent);
+
+                    //aspetto un secondo e stampo la risposta
+                    setTimeout(function() {
+                        //creo l'oggetto messaggio di risposta da aggiungere in coda all'array messages
+                        const messageReceived = {date: time.getHours() + ':' + time.getMinutes(), message: 'ok', status: 'received'};
+
+                        //aggiungo il messaggio
+                        temp.messages.push(messageReceived);
+                    }, 1000);
+                }
+            }
+        },
+        /* funzione che filtra i contatti dalla lista e visualizza solo quelli che iniziano con quanto digitato */
+        cerca(search) {
+            for (let i = 0; i < this.contacts.length; i++) {
+                let temp = this.contacts[i]; //variabile temporanea (con visibilità solo nel for) per non scrivere sempre this.contacts[i]
+                if (temp.name.toLowerCase().startsWith(search.toLowerCase())) {
+                    temp.filter = true;
+                } else {
+                    temp.filter = false;
+                }
+            }
+        }
+    }
+
 }).mount("#app")
